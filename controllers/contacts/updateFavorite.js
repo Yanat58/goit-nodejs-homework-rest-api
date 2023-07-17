d2774adb330fb2c9f710c7f52ff665ea7275af6e
Tetiana Karlova <tanyalyt80@gmail.com>
2023-07-17 16:52:12 +0300

    update

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -8,7 +8,7 @@ const updateFavorite = async (req, res) => {
   if (!result) {
     throw HttpError(404, 'Not found');
   }
-  res.json(result);
+  res.status(200).json(result);
 };
 
 module.exports = updateFavorite;
