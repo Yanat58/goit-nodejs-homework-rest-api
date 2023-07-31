72f500f17b42bb9d978ae774cc0b293389d0eac4
Tetiana Karlova <tanyalyt80@gmail.com>
2023-07-31 22:05:25 +0300

    update

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,18 +1,18 @@
 const { Contact } = require('../../models/contacts');
 
 const listContacts = async (req, res) => {
-  const { _id } = req.user;
+  const { _id: owner } = req.user;
   const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
   const result = await Contact.find(
     !favorite
-      ? ({ owner: _id },
+      ? ({ owner },
         {
           skip,
           limit: +limit,
         })
-      : { owner: _id, favorite }
+      : { owner, favorite }
   ).populate('owner', 'email');
 
   res.status(200).json(result);
